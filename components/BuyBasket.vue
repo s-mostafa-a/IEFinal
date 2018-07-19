df939df95d3513e89fb77eb19236ebb746a8f648
mostafa <mostafa.ahmadi1375@yahoo.com>
2018-07-20 01:09:52 +0430

    detail done

--- components/BuyBasket.vue
+++ components/BuyBasket.vue
@@ -1,4 +1,5 @@
 <template>
+<div>
     <b-container fluid class="p-4 ">
       <b-row id="brow" style="direction: rtl;">
           <b-col> مشخصات محصول </b-col>
@@ -6,29 +7,30 @@
           <b-col> قیمت </b-col>
       </b-row>
       <b-row id="brow2" style="direction: rtl;">
-          <b-col>
-              <div style="display: flex;">
-            <b-col>
+          <b-col style="display: flex;">
+              <b-col>
                      <img :src="img" width="80px" height="80px">
               </b-col>
-              <b-col style="font-size:8px">
+              <b-col style="font-size:8px; float: right;">
                    {{brand}}<br>
                    {{prname}}<br>
                    {{color}}<br>
                    {{size}}<br>
                    {{code}}
                </b-col>
-               </div>
+
           </b-col>
-          <b-col style="display: flex;">
+          <b-col style="margin: auto;">
               {{no}}
           </b-col>
-          <b-col> 
+          <b-col style="margin: auto;"> 
               {{price}}
           </b-col>
       </b-row>
-
       </b-container>
+<button type="button" class="tajrobe">ثبت و مرحله بعد</button>
+
+</div>
 </template>
 <script>
 export default {
@@ -57,12 +59,22 @@ export default {
     font-family: "my_iran_sans";
 }
 
+.tajrobe{
+    float: left;
+    font-size: 100%;
+    border: 1px solid #6464ff;
+    border-radius: 25px;
+    width: 15%;
+    color: #ffffff;
+    -webkit-transition: background-color 0.4s ease;
+    transition: background-color 0.4s ease;
+    background-color: #3232ff;
+}
 #brow{
     background-color: gray;
     color: white;
 }
 #brow2{
-    float: right;
     background-color: white;
     color: black;
 }
