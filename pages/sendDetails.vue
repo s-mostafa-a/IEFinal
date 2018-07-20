c51bc2e6a22788405181388319455d52e2cd3ae3
mostafa <mostafa.ahmadi1375@yahoo.com>
2018-07-20 14:04:32 +0430

    send details joda shod

---
+++ pages/sendDetails.vue
@@ -0,0 +1,34 @@
+<template>
+  <section class="container">
+    <div>
+    <login-part/>
+      <address-card/>
+      <buttom-bar/>
+      </div>
+  </section>
+</template>
+
+<script>
+import ButtomBar from '~/components/ButtomBar.vue'
+import TopBar from '~/components/TopBar.vue'
+import LoginPart from '~/components/LoginPart.vue'
+import UserInfo from '~/components/UserInfo.vue'
+import AddressCard from '~/components/AddressCard.vue'
+
+
+export default {
+  components: {
+    ButtomBar,
+    TopBar,
+    LoginPart,
+    UserInfo,
+    AddressCard
+
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
+
