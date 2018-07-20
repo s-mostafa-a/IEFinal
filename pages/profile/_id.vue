24ac96b62088457decb56cb7b27d89dbc4b08f62
mostafa <mostafa.ahmadi1375@yahoo.com>
2018-07-20 19:13:16 +0430

    profile not finished

--- pages/profile/_id.vue
+++ pages/profile/_id.vue
@@ -8,22 +8,34 @@
         <br>
 
     <div>
-    <b-btn @click="showCollapse = !showCollapse"
-           :class="showCollapse ? 'collapsed' : null"
+    <b-btn @click="showCollapse1 = !showCollapse1"
+           :class="showCollapse1 ? 'collapsed' : null"
            aria-controls="collapse4"
-           :aria-expanded="showCollapse ? 'true' : 'false'" style="float: right;">
+           :aria-expanded="showCollapse1 ? 'true' : 'false'" style="float: right;">
         آدرس‌ها
     </b-btn>
-    <b-collapse class="mt-2" v-model="showCollapse" id="collapse4">
+    <b-collapse class="mt-2" v-model="showCollapse1" id="collapse4">
         <br>
-                <br>
-
         <br>
-
+        <br>
         <address-card/>
     </b-collapse>
     </div>
-
+    <br>
+    <br>
+    <br>
+    <b-btn @click="showCollapse = !showCollapse"
+           :class="showCollapse ? 'collapsed' : null"
+           aria-controls="collapse3"
+           :aria-expanded="showCollapse ? 'true' : 'false'" style="float: right;">
+        سفارشات
+    </b-btn>
+    <b-collapse class="mt-2" v-model="showCollapse" id="collapse3">
+        <br>
+        <br>
+        <br>
+        <buy-basket/>
+    </b-collapse>
 
         <br>
         <br>
@@ -33,12 +45,13 @@
       </div>
   </section>
 </template>
-
 <script>
 import ButtomBar from '~/components/ButtomBar.vue'
 import TopBar from '~/components/TopBar.vue'
 import UserInfo from '~/components/UserInfo.vue'
 import AddressCard from '~/components/AddressCard.vue'
+import BuyBasket from '~/components/BuyBasket.vue'
+
 
 
 
@@ -46,7 +59,9 @@ import AddressCard from '~/components/AddressCard.vue'
 export default {
     data(){
         return{
-            showCollapse: true
+            showCollapse1: true,
+            showCollapse: false
+
         }
     },
   components: {
@@ -54,6 +69,7 @@ export default {
     TopBar,
     UserInfo,
     AddressCard,
+    BuyBasket
 
   }
 }
